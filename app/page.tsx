c304ecf0ab9cfc126d57d23a3cce84dad871ed93
destrooooo <clementsilva@live.fr>
2026-02-11 13:38:41 +0100

    added grid composition & drag allowance between x and y axis

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,268 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { area } from "framer-motion/client";
 
 export default function Home() {
+  //  12*[1,1], 5*[2,1], 1*[2,2]
+  // grid-area: rowStart / colStart / rowEnd / colEnd
+
+  const levels: Record<number, string[]> = {
+    1:
+      // prettier-ignore
+      [
+        "yellow","yellow","","",
+        "yellow","yellow","yellow","yellow",
+        "yellow","yellow","yellow","yellow",
+        "red","red","blue","yellow",
+        "red","red","blue","yellow"
+      ],
+    2:
+      // prettier-ignore
+      [
+        "yellow","yellow","yellow","yellow",
+        "","","blue","yellow",
+        "red","red","blue","yellow",
+        "red","red","yellow","yellow",
+        "yellow","yellow","yellow","yellow"
+      ],
+    3:
+      // prettier-ignore
+      [
+        "blue","yellow","yellow","blue",
+        "blue","green","green","blue",
+        "blue","red","red","",
+        "blue","red","red","blue",
+        "yellow","","yellow","blue"
+      ],
+  };
+
+  function parseLevel(layout: string[]) {
+    const pieces = [];
+    const visited = new Set<number>();
+    let piecedId = 1;
+
+    for (let row = 0; row < 5; row++) {
+      for (let col = 0; col < 4; col++) {
+        const index = row * 4 + col;
+        const color = layout[index];
+        let colSpan = 1;
+        let rowSpan = 1;
+
+        if (color === "" || visited.has(index)) continue;
+        if (color === "yellow") {
+          colSpan = 1;
+          rowSpan = 1;
+          pieces.push({
+            id: piecedId++,
+            area: `${row + 1}/${col + 1}/${row + rowSpan + 1}/${col + colSpan + 1}`,
+            color: color,
+          });
+          visited.add(index);
+        } else if (color === "red") {
+          colSpan = 2;
+          rowSpan = 2;
+          pieces.push({
+            id: piecedId++,
+            area: `${row + 1}/${col + 1}/${row + rowSpan + 1}/${col + colSpan + 1}`,
+            color: color,
+          });
+          for (let r = 0; r < rowSpan; r++) {
+            for (let c = 0; c < colSpan; c++) {
+              const cellIndex = (row + r) * 4 + (col + c);
+              visited.add(cellIndex);
+            }
+          }
+        } else if (color === "blue") {
+          colSpan = 1;
+          rowSpan = 2;
+          pieces.push({
+            id: piecedId++,
+            area: `${row + 1}/${col + 1}/${row + rowSpan + 1}/${col + colSpan + 1}`,
+            color: color,
+          });
+          for (let r = 0; r < rowSpan; r++) {
+            for (let c = 0; c < colSpan; c++) {
+              const cellIndex = (row + r) * 4 + (col + c);
+              visited.add(cellIndex);
+            }
+          }
+        } else if (color === "green") {
+          colSpan = 2;
+          rowSpan = 1;
+          pieces.push({
+            id: piecedId++,
+            area: `${row + 1}/${col + 1}/${row + rowSpan + 1}/${col + colSpan + 1}`,
+            color: "blue",
+          });
+          for (let r = 0; r < rowSpan; r++) {
+            for (let c = 0; c < colSpan; c++) {
+              const cellIndex = (row + r) * 4 + (col + c);
+              visited.add(cellIndex);
+            }
+          }
+        }
+      }
+    }
+    console.log(pieces);
+    return pieces;
+  }
+
+  const [pieces, setPieces] = useState(
+    calculateDraggablePieces(parseLevel(levels[3])),
+  );
+
+  function calculateDraggablePieces(pieces) {
+    // 1. Créer grille 5x4 remplie de false
+    const grid = Array(5)
+      .fill(null)
+      .map(() => Array(4).fill(false));
+
+    // 2. Marquer toutes les cellules occupées par toutes les pièces
+    pieces.forEach((piece) => {
+      // TODO: parser gridArea pour extraire rowStart, colStart, rowEnd, colEnd
+      // grid-area: rowStart / colStart / rowEnd / colEnd
+      const [rowStart, colStart, rowEnd, colEnd] = piece.area
+        .split("/")
+        .map((n) => parseInt(n.trim()));
+
+      // TODO: double boucle pour marquer chaque cellule occupée
+      for (let row = rowStart - 1; row < rowEnd - 1; row++)
+        for (let col = colStart - 1; col < colEnd - 1; col++)
+          grid[row][col] = true;
+    });
+
+    // 3. Pour chaque pièce, calculer draggable.x et draggable.y
+    return pieces.map((piece) => {
+      //parse gridArea
+
+      const [rowStart, colStart, rowEnd, colEnd] = piece.area
+        .split("/")
+        .map((n) => parseInt(n.trim()));
+
+      const canMoveUp = (() => {
+        //check if on edge
+        if (rowStart - 1 <= 0) {
+          return false;
+        }
+
+        // check if available space above
+        for (let col = colStart - 1; col < colEnd - 1; col++) {
+          if (grid[rowStart - 2][col] === true) {
+            return false;
+          }
+        }
+        return true;
+      })();
+
+      // check if all cell have space beneath
+      const canMoveDown = (() => {
+        //check if on edge
+        if (rowEnd === 6) {
+          return false;
+        }
+
+        //check if available space beneath
+        for (let col = colStart - 1; col < colEnd - 1; col++) {
+          if (grid[rowEnd - 1][col] === true) {
+            return false;
+          }
+        }
+        return true;
+      })();
+
+      // check if all cell have space on left
+      const canMoveLeft = (() => {
+        //check if on edge
+        if (colStart - 1 <= 0) {
+          return false;
+        }
+
+        //check if available space on left
+        for (let row = rowStart - 1; row < rowEnd - 1; row++) {
+          if (grid[row][colStart - 2] === true) {
+            return false;
+          }
+        }
+        return true;
+      })();
+
+      // check if all cell have space on right
+      const canMoveRight = (() => {
+        //check if on edge
+        if (colEnd === 5) {
+          return false;
+        }
+
+        //check if available space on right
+        for (let row = rowStart - 1; row < rowEnd - 1; row++) {
+          if (grid[row][colEnd - 1] === true) {
+            return false;
+          }
+        }
+        return true;
+      })();
+
+      // Calculer les valeurs x et y
+      let x = 0; // 0 = bloqué
+      if (canMoveLeft && canMoveRight)
+        x = 2; // les deux
+      else if (canMoveLeft) x = -1;
+      else if (canMoveRight) x = 1;
+
+      let y = 0;
+      if (canMoveUp && canMoveDown) y = 2;
+      else if (canMoveUp) y = -1;
+      else if (canMoveDown) y = 1;
+
+      return {
+        ...piece,
+        draggable: { x, y },
+      };
+    });
+  }
+
+  const testMove = () => {
+    setPieces((prev) =>
+      prev.map((piece) =>
+        piece.id === 8 ? { ...piece, area: "3/4/5/5" } : piece,
+      ),
+    );
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      <button onClick={testMove} className="mb-4 px-4 py-2 bg-blue-500">
+        Test Move Piece 1
+      </button>
+      <div className=" relative w-100 h-125 border grid grid-cols-4 grid-rows-5 gap-2 ">
+        <div className="absolute w-[195px] h-[194.4px] bottom-0 left-1/2 bg-red-500 opacity-25 -translate-x-1/2 -z-30"></div>
+        {pieces.map((piece) => (
+          <motion.div
+            layout
+            key={piece.id}
+            drag={
+              piece.draggable.x !== 0 && piece.draggable.y !== 0
+                ? true
+                : piece.draggable.x !== 0
+                  ? "x"
+                  : piece.draggable.y !== 0
+                    ? "y"
+                    : false
+            }
+            style={{
+              gridArea: piece.area,
+              background: piece.color,
+              cursor:
+                piece.draggable.x !== 0 || piece.draggable.y !== 0
+                  ? "grab"
+                  : "not-allowed",
+            }}
+            className="card"
+            data-id={piece.id}
+          ></motion.div>
+        ))}
+      </div>
+    </>
   );
 }
